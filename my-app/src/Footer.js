5401280f0bbb570dd19c54a1d9c09d4134d0d496
enes-triple <enes.gokce.developer@gmail.com>
2020-08-28 23:05:46 -0700

    footer version 2

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -3,18 +3,22 @@ import { MDBIcon, MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
 const FooterPagePro = () => {
   return (
-    <MDBFooter color="stylish-color-dark" className="page-footer font-small pt-4">
+    <MDBFooter className="page-footer footer font-small pt-4">
       <MDBContainer fluid className="text-center text-md-left">
         <MDBRow>
           <MDBCol md="3">
             <MDBContainer>
-                <h5 className="text-uppercase mb-4 mt-3 font-weight-bold">
+                <a class="logo-area" href="#!">
+                    <img src="https://reverehome.ca/assets/icons/footerLogo.png" alt="logo" />
+                    <h5 className="font-weight-bold mt-2">REVERE</h5>
+                </a>
+                <h6 className="text-uppercase mb-3 mt-4 font-weight-bold">
                     Real Estate is the Where of our stories
-                </h5>
+                </h6>
                 <p>
                     We are focused on building the platform for people and places - To connect the world to where we live, grow, and become.
                 </p>
-                <a href="#!" className="btn btn-md btn-primary mb-3" style={{ marginLeft: 0 }}>
+                <a href="#!" className="btn btn-md btn-warning mt-3 mb-3" style={{ marginLeft: 0 }}>
                     JOIN REVERE <MDBIcon icon="arrow-right" className="ml-2" />
                 </a>
             </MDBContainer>
@@ -99,11 +103,11 @@ const FooterPagePro = () => {
             <h5 className="mb-1">Mobile Apps</h5>
           </li>
           <li className="list-inline-item">
-            <a href="#!" className="btn btn-md btn-danger btn-rounded">
-              iOS<MDBIcon fab icon="apple" size="lg" className="ml-2" />
+            <a href="#!" className="btn btn-md btn-outline-white btn-rounded">
+                <MDBIcon fab icon="apple" size="lg" className="mr-2" />iOS
             </a>
-            <a href="#!" className="btn btn-md btn-info btn-rounded">
-              Android<MDBIcon fab icon="android" size="lg" className="ml-2" />
+            <a href="#!" className="btn btn-md btn-outline-white btn-rounded">
+                <MDBIcon fab icon="android" size="lg" className="mr-2" />Android
             </a>
           </li>
         </ul>
@@ -112,27 +116,27 @@ const FooterPagePro = () => {
       <div className="text-center">
         <ul className="list-unstyled list-inline">
           <li className="list-inline-item">
-            <a className="btn-floating btn-sm btn-fb mx-1">
+            <a className="btn-floating btn-sm btn-icon mx-1">
               <i className="fab fa-facebook-f"> </i>
             </a>
           </li>
           <li className="list-inline-item">
-            <a className="btn-floating btn-sm btn-tw mx-1">
+            <a className="btn-floating btn-sm btn-icon mx-1">
               <i className="fab fa-twitter"> </i>
             </a>
           </li>
           <li className="list-inline-item">
-            <a className="btn-floating btn-sm btn-gplus mx-1">
+            <a className="btn-floating btn-sm btn-icon mx-1">
               <i className="fab fa-google-plus"> </i>
             </a>
           </li>
           <li className="list-inline-item">
-            <a className="btn-floating btn-sm btn-li mx-1">
+            <a className="btn-floating btn-sm btn-icon mx-1">
               <i className="fab fa-linkedin-in"> </i>
             </a>
           </li>
           <li className="list-inline-item">
-            <a className="btn-floating btn-sm btn-dribbble mx-1">
+            <a className="btn-floating btn-sm btn-icon mx-1">
               <i className="fab fa-dribbble"> </i>
             </a>
           </li>
@@ -140,7 +144,7 @@ const FooterPagePro = () => {
       </div>
       <div className="footer-copyright text-center py-3">
         <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Revere. All rights reserved. <a href="https://www.MDBootstrap.com">Made with Love In Canada</a>
+          &copy; {new Date().getFullYear()} Revere.&ensp;All rights reserved.&ensp;<a href="https://www.MDBootstrap.com">Made with Love In Canada</a>
         </MDBContainer>
       </div>
     </MDBFooter>
